d82031a33f9c14d2c8d4299cefde7d2530513280
leopedersen <leo-peder@hotmail.com>
2021-09-28 17:24:10 +0200

    REVERT test shouldReturnStatusCode (fail)

--- src/test/java/no/kristiania/http/HttpClientTest.java
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -6,12 +6,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class HttpClientTest {
     @Test
-    void dummyTest(){
-        assertEquals(42, 6*7);
-    }
-    @Test
-    void shouldReturnStatusCode(){
-        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
-        assertEquals(200, client.getStatusCode());
+    void dummyTest() {
+        assertEquals(42, 6 * 7);
     }
 }
+
