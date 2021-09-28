90eba989860b05bd5ac62c8ec7ab50640ad518a7
leopedersen <leo-peder@hotmail.com>
2021-09-28 18:01:09 +0200

    test shouldReturn404StatusCode (refactored)

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -7,11 +7,37 @@ import java.nio.charset.StandardCharsets;
 
 public class HttpClient {
 
-    public HttpClient(String Host, int port, String requestTarget) {
+    private final int statusCode;
+
+    public HttpClient(String host, int port, String requestTarget) throws IOException {
+        Socket socket = new Socket(host, port);
+
+        socket.getOutputStream().write(
+                ("GET " + requestTarget + " HTTP/1.1\r\n" +
+                        "Connection: close\r\n" +
+                        "Host: " + host + "\r\n" +
+                        "\r\n").getBytes()
+        );
+
+
+
+        String statusLine = readLine (socket);
+
+        this.statusCode = Integer.parseInt(statusLine.split(" ")[1]);
     }
 
+    private String readLine(Socket socket) throws IOException {
+        StringBuilder result = new StringBuilder();
+        InputStream in = socket.getInputStream();
+        int c;
+        while ((c = in.read()) != -1 && c != '\r') {
+            result.append((char) c);
+        }
+        return result.toString();
+        }
+
     public int getStatusCode() {
-        return 200;
+        return statusCode;
     }
 
     public static void main(String[] args) throws IOException {
@@ -20,15 +46,12 @@ public class HttpClient {
 
         socket.getOutputStream().write(
                 ("GET /html HTTP/1.1\r\n" +
+                        "Connection: close\r\n" +
                         "Host: httpbin.org\r\n" +
                         "\r\n").getBytes()
         );
 
-        InputStream in = socket.getInputStream();
-        int c;
-        while ((c = in.read()) != -1){
-            System.out.print((char)c);
-        }
+
 
 
     }
