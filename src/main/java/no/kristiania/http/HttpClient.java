6457a0f1b685429fb03ccf73b4514fc932679367
leopedersen <leo-peder@hotmail.com>
2021-09-28 15:44:24 +0200

    Hello world

---
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -0,0 +1,16 @@
+package no.kristiania.http;
+
+import java.nio.charset.StandardCharsets;
+
+public class HttpClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        /*Socket socket = new socket("httpbin.org", 80);
+        String request = ....
+        socket.getOutputStream().write(request.getBytes());
+    }
+
+         */
+    }
+}
